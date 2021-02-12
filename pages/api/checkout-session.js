2ff0c63f5d199cd41aefae3b11800e66b1c9ee88
josehower <josehowerupn@gmail.com>
2021-02-12 12:09:16 +0100

    Add format fixes

--- pages/api/checkout-session.js
+++ pages/api/checkout-session.js
@@ -1,7 +1,7 @@
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { sessionId } = req.query;
   const session = await stripe.checkout.sessions.retrieve(sessionId);
   res.send(session);
--- pages/api/create-checkout-session.js
+++ pages/api/create-checkout-session.js
@@ -1,35 +1,35 @@
 import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-  const domainURL = "http://localhost:3000";
+    const domainURL = 'http://localhost:3000';
 
-  const { quantity, locale } = req.body;
+    const { quantity, locale } = req.body;
 
-  const pmTypes = ["card"]
+    const pmTypes = ['card'];
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: pmTypes,
-    mode: 'payment',
-    locale: locale,
-    line_items: [
-      {
-        price: process.env.PRICE,
-        quantity: quantity,
-      },
-    ],
-    // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
-    success_url: `${domainURL}/success?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${domainURL}/canceled`,
-  });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: pmTypes,
+      mode: 'payment',
+      locale: locale,
+      line_items: [
+        {
+          price: process.env.PRICE,
+          quantity: quantity,
+        },
+      ],
+      // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+      success_url: `${domainURL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${domainURL}/canceled`,
+    });
 
-  res.send({
-    sessionId: session.id,
-  })} else{
     res.send({
-      error: "Method need to be POST",
+      sessionId: session.id,
+    });
+  } else {
+    res.send({
+      error: 'Method need to be POST',
     });
   }
 }
