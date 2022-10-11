f0dc04d66208763eea951ba98ec8a5fc5950e6e1
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-10-11 11:12:23 +0200

    Update content

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
 import { useState } from 'react';
-import Product from '../components/Product';
-import Subscription from '../components/Subscription';
+import { Counter } from '../components/Counter';
 
-export default function Home() {
+export default function Home(props) {
   const [productQuantity, setProductQuantity] = useState(1);
+
   return (
     <div>
       <div>
-        <Product
-          productQuantity={productQuantity}
-          setProductQuantity={setProductQuantity}
-        />
+        <h1>Nice Product</h1>
+        <p>
+          This is a one time payment <span>product</span>.
+        </p>
+        <div>
+          <img
+            alt="Random asset from Picsum"
+            src={props.image || '/images/no-image.png'}
+          />
+          <Counter count={productQuantity} setCount={setProductQuantity} />
+        </div>
         {/* fix this */}
         <button>Buy for XX - FIX ME</button>
       </div>
       <div>
-        <Subscription />
+        <h1>Subscription Plan</h1>
+        <p>
+          This is a recurring payment <span>product</span>.
+        </p>
+        <img alt="magazine" src={props.image || '/images/no-image.png'} />
         {/* fix this */}
         <button>Buy for XX - FIX ME</button>
       </div>
