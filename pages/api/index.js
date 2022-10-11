038facf6917aca4e3bd228fdc4c3129abc1ae55f
José Fernando Höwer Barbosa <josehowerupn@gmail.com>
2022-10-11 15:03:29 +0200

    Add lecture content

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,1 +1,7 @@
 // add api code here
+
+export default async function handler(request, response) {
+  response
+    .status(200)
+    .json({ sessions: `http://${request.headers.host}/api/sessions` });
+}
